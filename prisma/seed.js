b4ea05f96e79b61e9465ac03ad9c2b21f1c8f7e2
yc530 <yoonachoi530@gmail.com>
2023-11-01 16:35:25 -0400

    created and added data, models

---
+++ prisma/seed.js
@@ -0,0 +1,50 @@
+const prisma = require("../prisma");
+
+const seed = async () => {
+  const authorData = await prisma.author.create({
+    data: {
+      name: "Yoona Choi",
+      books: {
+        create: [
+          { title: "I Love My Chihuahua" },
+          { title: "I Love My Friends & Family" },
+          { title: "Who Cares? I do! :("}
+        ],
+      },
+    },
+  });
+
+  await prisma.author.create({
+    data: {
+      name: "Carrie Bradshaw",
+      books: {
+        create: [
+          { title: "Sex & The City" },
+          { title: "Sex & The City Two" },
+          {title: "And Just Like That"}
+        ],
+      },
+    },
+  });
+
+  await prisma.author.create({
+    data: {
+      name: "Junie B Jones",
+      books: {
+        create: [
+          { title: "I Hate My Family!" },
+          { title: "Where's My Diary?" },
+          {title: "Grumpy Kids Read Men"}
+        ],
+      },
+    },
+  });
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
